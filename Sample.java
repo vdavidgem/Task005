6920070467444ee1a5fae8584e77d20b8e34af35
vdavidgem <47139426+vdavidgem@users.noreply.github.com>
2019-06-10 18:17:26 +0530

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Sample {
+	public static void main(String args[]) {
+		System.out.println("Hello Java");
+		System.out.println("hello Jenkins");
+		System.out.println("Hello world");
+		System.out.println("HDFS");
+		System.out.println("HI@@XXXXXXX");
+	}
+}
